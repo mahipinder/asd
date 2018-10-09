067e6552c73ac2c717f6dc37e0bbb110a1cb97b2
mahipinder <msdsworld001@gmail.com>
2018-10-09 19:18:44 +0530

    product python

---
+++ details.py
@@ -0,0 +1,11 @@
+#n="Mahipiner"
+#university_roll_number=1810992576
+#email="msdsworld001@gmail.com"
+#mobile=9041757057
+
+#print "User Details : \n Name : ",n,"\n Roll Number : ",university_roll_number,"\n Email Id ",email,"\n Contact",mobile
+
+string='Mahipinder'
+string1="Singh"
+string2="""@ TraineeBase """
+print string,string1,string2*2
---
+++ product.py
@@ -0,0 +1,8 @@
+b="burger"
+quan=5
+specs="Cheese,Aloo Tikki,Onion"
+prep="10 Mins"
+Xtime="5 Mins"
+Ttime="15 Mins"
+Price=500
+print "Product Info: \n Item",b,"\n Quantity:",quan,"\n Specifications:",specs,"\n Preparation:",prep,"\n Extra Time:",Xtime,"\n Total Time:",Ttime,"\n Price:",Price
